8d6d00893615ae8c130ec44cacc8af237c7f0113
Gary Riches <garyriches@me.com>
2015-09-22 20:35:35 +0100

    Initial Commit

---
+++ superfling/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  superfling
+//
+//  Created by Gary Riches on 22/09/2015.
+//  Copyright (c) 2015 Gary Riches. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ superfling/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  superfling
+//
+//  Created by Gary Riches on 22/09/2015.
+//  Copyright (c) 2015 Gary Riches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
