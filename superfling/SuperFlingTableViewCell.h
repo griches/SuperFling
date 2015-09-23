d4835a7fe31d33bb31006eb579f66b7255b1ce61
Gary Riches <garyriches@me.com>
2015-09-23 12:35:52 +0100

    Basic table view cell implemented

---
+++ superfling/SuperFlingTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  SuperFlingTableViewCell.h
+//  superfling
+//
+//  Created by Gary Riches on 23/09/2015.
+//  Copyright (c) 2015 Gary Riches. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuperFlingTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) IBOutlet UIImageView *cellImage;
+@property (nonatomic, strong) IBOutlet UILabel *cellTitle;
+@property (nonatomic, strong) IBOutlet UIActivityIndicatorView *cellActivityIndicator;
+
+@end
---
+++ superfling/SuperFlingTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SuperFlingTableViewCell.m
+//  superfling
+//
+//  Created by Gary Riches on 23/09/2015.
+//  Copyright (c) 2015 Gary Riches. All rights reserved.
+//
+
+#import "SuperFlingTableViewCell.h"
+
+@implementation SuperFlingTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
